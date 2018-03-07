651feaeceda7e092335a80a99c171d0c594c91ba
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-07 19:15:53 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,19 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+# def help
+#   puts
+#   "I accept the following commands:
+# - help : displays this help message
+# - list : displays a list of songs you can play
+# - play : lets you choose a song to play
+# - exit : exits this program"
+# end
+
+def list(arr)
+  new_arr = []
+
+  arr.each_with_index do |each, index|
+    binding.pry
+  end
+end
