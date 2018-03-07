ac9159db77a64dae35aea8605c1e420dbacdcf09
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-07 19:42:31 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -23,7 +23,9 @@ def play(songs)
   input = gets.chomp
 
   songs.each_with_index do |song, index|
-  if songs.include? input || input.to_i == index+1
+  if songs.include? input
+    puts "Playing #{song}"
+  elsif input.to_i - 1 == index
     puts "Playing #{song}"
   else
     puts "Invalid input, please try again"
